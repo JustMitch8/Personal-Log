a8d27f910617136e3a95fc28969a9163f1a64093
JustMitch8 <130960014+JustMitch8@users.noreply.github.com>
2026-08-04 21:49:15 +1000

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+/* ===========================================
+   Contact Tracker
+   config.js
+
+   Replace the two values below with your own
+   Supabase project details.
+
+   These are safe to expose in a frontend app.
+
+   NEVER put your Service Role Key here.
+   =========================================== */
+
+const CONFIG = {
+
+    supabaseUrl:
+        "https://dsuuchfjrrjprlkxzxdi.supabase.co/rest/v1/",
+
+    supabaseAnonKey:
+        "sb_publishable_RFEIObgge1RdZfUMbQ69nw_Ec0SOp5e"
+
+};
